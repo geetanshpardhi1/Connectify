ea872eb81166c2e31ce1214854d4371aa901cf3e
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2024-03-12 14:49:25 +0530

    sets the new configuration project

---
+++ backend/connectify/accounts/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+
+#  Custom User Manager
+class UserManager(BaseUserManager):
+  def create_user(self, email, username, password=None, password2=None):
+      """
+      Creates and saves a User with the given email, username, and password.
+      """
+      if not email:
+          raise ValueError('User must have an email address')
+      if not username:
+          raise ValueError('User must have an username')
+
+      user = self.model(
+          email=self.normalize_email(email),
+          username=username,
+      )
+
+      user.set_password(password)
+      user.save(using=self._db)
+      return user
+
+  def create_superuser(self, email, username, password=None):
+      """
+      Creates and saves a superuser with the given email, username, and password.
+      """
+      user = self.create_user(
+          email,
+          password=password,
+          username=username,
+      )
+      user.is_admin = True
+      user.save(using=self._db)
+      return user
+
+#  Custom User Model
+class User(AbstractBaseUser):
+  email = models.EmailField(
+      verbose_name='Email',
+      max_length=255,
+      unique=True,
+  )
+  username = models.CharField(max_length=200,unique=True)
+  is_active = models.BooleanField(default=True)
+  is_admin = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  objects = UserManager()
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
+
+  def __str__(self):
+      return self.username
+
+  def has_perm(self, perm, obj=None):
+      return self.is_admin
+
+  def has_module_perms(self, app_label):
+      return True
+
+  @property
+  def is_staff(self):
+      return self.is_admin
