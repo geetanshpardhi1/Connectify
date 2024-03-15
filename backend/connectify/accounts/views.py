0f93d577386cadc4d24a9d41c0d4eb0f46de470d
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2024-03-15 17:29:56 +0530

    changes in sendPassRestMail serializer and view to send link as json object

--- backend/connectify/accounts/views.py
+++ backend/connectify/accounts/views.py
@@ -67,7 +67,8 @@ class SendPasswordResetEmailView(APIView):
     def post(self,request,format=None):
         serializer =SendPasswordResetEmailSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            return Response({'msg':'Password Reset Link sent, Please check your email'},status=status.HTTP_200_OK) 
+            reset_link = serializer.validated_data.get('reset_link')
+            return Response({'reset_link': reset_link,'msg':'Password Reset Link sent, Please check your email'},status=status.HTTP_200_OK) 
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
     
 class UserPasswordResetView(APIView):
