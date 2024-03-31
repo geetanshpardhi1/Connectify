a72cf6d330a829122cf1468f139287b3b1dc568b
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2024-03-31 11:27:59 +0530

    testing commit for git

--- backend/connectify/accounts/serializers.py
+++ backend/connectify/accounts/serializers.py
@@ -115,8 +115,4 @@ class UserPasswordResetSerializer(serializers.Serializer):
         except DjangoUnicodeDecodeError as identifier:
             PasswordResetTokenGenerator.check_token(user,token)
             raise serializers.ValidationError("Token is corrupted or expired")
-<<<<<<< HEAD
-
-=======
-bb
->>>>>>> da08021eae1ef365e7cc4bc2672c2a199d256e88
+        
